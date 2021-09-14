91c0a578fca5edb8a2065ee897b0d74ea4b15dda
Andreas <adde.engberg@gmail.com>
2021-09-14 16:40:28 +0200

    Fixed problem with ray and triangle

--- header/Ray.h
+++ header/Ray.h
@@ -4,12 +4,34 @@
 
 class Ray {
 public:
-	Ray(Vertex& start, Vertex& end, Color& color) : startPoint{ start }, endPoint{ end }, rayColor{ color } {}
-	Ray(Vertex& start, Vertex& end) : startPoint{ start }, endPoint{ end }, rayColor{} {}
 	
+	Ray(const Vertex& start, const Vertex& end/*,const Triangle* _loc = nullptr */, const Color& c = Color{}) :
+		startPoint{ start }, endPoint{ end }, rayColor{ c } {
+		/*endPointLocation = _loc ? new Triangle{*_loc} : nullptr ; */
+	}
+
+	//Ray(const Ray& r) : startPoint{ r.startPoint }, endPoint{ r.endPoint }, rayColor{ r.rayColor } {
+	//	endPointLocation = r.endPointLocation ? new Triangle{ *r.endPointLocation } : nullptr;
+	//}
+
+	//~Ray() {
+	//	delete endPointLocation;
+	//}
+
+	////copy swap idiom
+	//Ray& operator=(Ray rhs) {
+	//	std::swap(endPointLocation, rhs.endPointLocation);
+	//	std::swap(startPoint, rhs.startPoint);
+	//	std::swap(endPoint, rhs.endPoint);
+	//	std::swap(rayColor, rhs.rayColor);
+	//	return *this;
+	//}
+
 	Vertex startPoint, endPoint;
 	Color rayColor;
 	//ref to triangle on which endpoint is located
+	glm::vec3 intersectionPoint;
+	//eventuellt inkludera importance i ray obj
 
 private:
 
--- header/Triangle.h
+++ header/Triangle.h
@@ -1,21 +1,31 @@
 #pragma once
 #include "glm\glm.hpp";
+#include "Direction.h"
 #include "Vertex.h"
 #include "Color.h"
-#include "Direction.h"
 #include "Ray.h"
 
-class Triangle{
+class Triangle {
 public:
-
 	Triangle() = default;
-	Triangle(Vertex vertex1, Vertex vertex2, Vertex vertex3, Color color, Direction normal) :
-		v1{vertex1}, v2{vertex2}, v3{vertex3}, color{color}, n{normal} {}
 
-	Vertex rayIntersection(Ray arg);
+	Triangle(const Vertex&  vertex1, const Vertex& vertex2, const Vertex&  vertex3, const Color& color) :
+		v1{ vertex1 }, v2{ vertex2 }, v3{ vertex3 }, color{ color } {
+		//Calculate normal direction by cross product of the triangle sides
+		glm::vec3 p1 = glm::vec3(v1.position.x, v1.position.y, v1.position.z);
+		glm::vec3 p2 = glm::vec3(v2.position.x, v2.position.y, v2.position.z);
+		glm::vec3 p3 = glm::vec3(v3.position.x, v3.position.y, v3.position.z);
+		glm::vec3 u = p1 - p2;
+		glm::vec3 v = p1 - p3;
+		
+		normal.direction = glm::cross(u, v);
+	}
+
+	Vertex* rayIntersection(Ray ray);
+	
 
 private:
 	Vertex v1, v2, v3;
 	Color color;
-	Direction n;
+	Direction normal;
 };
