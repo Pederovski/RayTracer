291da955c3fafc2bc27849b1bfd61afd54c24230
Peder <peder-d@live.se>
2021-09-14 10:26:51 +0200

    Pixel and Triangle class

--- header/Pixel.h
+++ header/Pixel.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include "Color.h"
+#include "Ray.h"
+
+class Pixel {
+public:
+
+	Pixel(Color color, Ray& ray) :
+		c{ color }, r{ ray } {}
+
+private:
+	Color c;
+	Ray& r;
+};
--- header/Triangle.h
+++ header/Triangle.h
@@ -1,1 +1,25 @@
 #pragma once
+#include "glm\glm.hpp";
+#include "Vertex.h"
+#include "Color.h"
+#include "Direction.h"
+#include "Ray.h"
+
+class Triangle{
+public:
+
+	Triangle(Vertex vertex1, Vertex vertex2, Vertex vertex3, Color color, Direction normal) :
+		v1{vertex1}, v2{vertex2}, v3{vertex3}, c{color}, n{normal} {}
+
+	Vertex rayIntersection(Ray arg) 
+	{
+		//möller-trumbore algorithm
+
+		return Vertex(0,0,0);
+	}
+
+private:
+	Vertex v1, v2, v3;
+	Color c;
+	Direction n;
+};
