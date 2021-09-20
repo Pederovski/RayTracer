38d9d42de4cebdd7c48962a060597d4ff4405f5b
Andreas <adde.engberg@gmail.com>
2021-09-20 17:40:00 +0200

    Added chrono time

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -2,9 +2,13 @@
 #include <glm/glm.hpp>
 #include "header/Vertex.h"
 #include "header/Camera.h"
+#include <chrono>
 
 int main()
 {
+
+    auto start = std::chrono::steady_clock::now();
+
     Vertex eye1 = Vertex{ -2, 0, 0 };
     Vertex eye2 = Vertex{ -1, 0,0 };
     Camera cam = Camera(eye1, eye2, false);
@@ -13,7 +17,10 @@ int main()
 
     cam.render(s);
 
-    glm::mat4 proj;
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
 
-    std::cout << "Yes" << "\n" << "Japp";
+    //glm::mat4 proj;
+    //std::cout << "Yes" << "\n" << "Japp";
 }
