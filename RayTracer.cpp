a9be8b216b0fb8506c2a1d30dc1e6085e9bfdb29
Andreas <adde.engberg@gmail.com>
2021-09-20 16:25:30 +0200

    Use eye2

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -7,7 +7,7 @@ int main()
 {
     Vertex eye1 = Vertex{ -2, 0, 0 };
     Vertex eye2 = Vertex{ -1, 0,0 };
-    Camera cam = Camera(eye1, eye2);
+    Camera cam = Camera(eye1, eye2, false);
 
     Scene s;
 
