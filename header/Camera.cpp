c84f0aec4211ea79782fdc632e4745bac48d94b9
Andreas <adde.engberg@gmail.com>
2021-09-20 17:39:28 +0200

    Potential fix for output rendering img

--- header/Camera.cpp
+++ header/Camera.cpp
@@ -4,44 +4,27 @@
 
 void Camera::render(Scene &_scene)
 {
-	Color c{ 255, 0.0, 0.0 };
-	//Vertex v1{ 0,0,0 };
-	//Vertex v2{ 0,0,0 };
-	//Ray ray{ v1,v2,c };
-
-	//Changes all pixel colors to red
-	//for (int i = 0; i < SIZE; i++) {
-	//	for (int j = 0; j < SIZE; j++) {
-	//		cameraplane[index(i, j)].c.color = c.color;
-	//	}
-	//}
-	
-	//
+	//Shot a ray through each pixel 
+	//Currently saves the first interaction with a triangle as that pixelcolor
 	for (int i = 0; i < SIZE; i++) {
 		for (int j = 0; j < SIZE; j++) {
 			//std::cout << cameraplane[index(0, 0)].c.color.r << " " << cameraplane[index(0, 0)].c.color.g << " " << cameraplane[index(0, 0)].c.color.b << "\n";
 			Color C = _scene.triangleIntersection(cameraplane[(index(i, j))].r);
 			cameraplane[index(i, j)].c = C;
-
 			//std::cout << "After: " << cameraplane[index(0, 0)].c.color.r << " " << cameraplane[index(0, 0)].c.color.g << " " << cameraplane[index(0, 0)].c.color.b << "\n";
-
 		}
 	}
 
-	
-
-	//cameraplane[index(400, 400)].r
-	//transfer pixel data from cameraplane to 
+	//transfer pixel data from cameraplane to image output
 	EasyBMP::Image img(SIZE, SIZE);
 	for (int i = 0; i < SIZE; i++) {
 		for (int j = 0; j < SIZE; j++) {
-			Color c = cameraplane[index(i, j)].c;
+			Color c = cameraplane[index(i,j)].c;
 			img.SetPixel(i, j, EasyBMP::RGBColor(c.color.r, c.color.g, c.color.b));
-
 		}
 	}
-	//easy write pic
-	img.Write("test.bmp");
+	//easy write image to file
+	img.Write("scene.bmp");
 }
 
 int Camera::index(int c, int r)
--- header/Camera.h
+++ header/Camera.h
@@ -10,14 +10,14 @@ const int SIZE = 800;
 class Camera {
 public:
 	Camera(Vertex& e1, Vertex& e2, bool useEye1 = true) : eye1{ e1 }, eye2{ e2 } {
-		std::cout << SIZE << "\n";
+		//std::cout << SIZE << "\n";
 		cameraplane.reserve(SIZE * SIZE);
 		float delta = 2.0f / SIZE; //pixel size width, height
 
 		for (int i = 0; i < SIZE; i++) {
 			for (int j = 0; j < SIZE; j++) {
 				//Calculate direction vector from eyepoint to the pixel
-				Vertex cameraplaneEndPoint = Vertex{ 0, i * delta - (1 - delta ), j * delta - (1 - delta) };
+				Vertex cameraplaneEndPoint = Vertex{ 0, j * delta - (1 - delta ), i * delta - (1 - delta) };
 				Ray dir = Ray{ useEye1 ? eye1 : eye2, cameraplaneEndPoint };
 				cameraplane.push_back(Pixel{ Color{}, dir });
 			}
