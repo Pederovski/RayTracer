665acd7e179e9169322524f6d5f3e0afdac2cac2
Andreas <adde.engberg@gmail.com>
2021-09-21 16:44:24 +0200

    Normalize rgb values

--- header/Camera.cpp
+++ header/Camera.cpp
@@ -14,13 +14,27 @@ void Camera::render(Scene &_scene)
 			//std::cout << "After: " << cameraplane[index(0, 0)].c.color.r << " " << cameraplane[index(0, 0)].c.color.g << " " << cameraplane[index(0, 0)].c.color.b << "\n";
 		}
 	}
+	//find max color value
+	double maxColorValue = std::numeric_limits<double>::min();
+	for (int i = 0; i < SIZE * SIZE; i++) {
+		double max = std::max(std::max(cameraplane[i].c.color.r, cameraplane[i].c.color.g), cameraplane[i].c.color.b);
+		if (maxColorValue < max)
+			maxColorValue = max;
+	}
+	//normalize color values between 0 -> 1
+	for (int i = 0; i < SIZE * SIZE; i++) {
+		cameraplane[i].c.color.r /= maxColorValue;
+		cameraplane[i].c.color.g /= maxColorValue;
+		cameraplane[i].c.color.b /= maxColorValue;
+	}
+
 
 	//transfer pixel data from cameraplane to image output
 	EasyBMP::Image img(SIZE, SIZE);
 	for (int i = 0; i < SIZE; i++) {
 		for (int j = 0; j < SIZE; j++) {
 			Color c = cameraplane[index(i,j)].c;
-			img.SetPixel(i, j, EasyBMP::RGBColor(c.color.r, c.color.g, c.color.b));
+			img.SetPixel(i, j, EasyBMP::RGBColor(c.color.r * 255, c.color.g * 255, c.color.b * 255));
 		}
 	}
 	//easy write image to file
